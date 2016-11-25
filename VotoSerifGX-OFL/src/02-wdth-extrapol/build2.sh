f1dd932f059163c3696d501e3c8870bd09b7aa51
Adam Twardoch <adam@twardoch.com>
2016-11-25 23:08:52 +0100

    rename

---
+++ VotoSerifGX-OFL/src/02-wdth-extrapol/build2.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env sh
+fontmake -m master_ufo/VotoSerifGX.designspace --keep-overlaps --production-names -o variable
---
+++ VotoSerifGX-OFL/src/03-opsz-extrapol-progress/build1.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env sh
+fontmake --g VotoSerif-MM.glyphs --keep-overlaps --production-names --use-afdko -o variable
